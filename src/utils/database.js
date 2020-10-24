b9e806e921c955b90bd36a0c975c5de7f591b2e1
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-23 23:50:12 -0300

    Documentação: Arquivo de configuração de banco de dados através do PG

---
+++ src/utils/database.js
@@ -0,0 +1,27 @@
+const { Client } = require('pg');
+
+require('dotenv').config();
+
+/**
+ * Dados de acesso ao banco de dados devem ser colocados no arquivo .env
+ */
+const database = new Client({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	password: process.env.DB_PW,
+	user: process.env.DB_USER,
+	database: process.env.DB_NAME,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
+
+database.connect((err) => {
+	if (err) {
+		console.error('connection error', err.stack);
+	} else {
+		console.log('connected');
+	}
+});
+
+module.exports = database;
