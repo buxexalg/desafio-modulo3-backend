62bf441db714bff5a024ea7cb50629c523deb671
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-23 23:52:05 -0300

    Documentação: Funções de gerenciamento de queries da tabela de usuários do banco de dados.

---
+++ src/repositories/queriesAuth.js
@@ -0,0 +1,17 @@
+const Database = require('../utils/database');
+
+
+/**
+ * Função responsável por fazer a query que retorna a senha em hash de um email específico.
+ */
+const usuarios = async (email) => {
+	const query = {
+		text: 'SELECT senha FROM users where email = $1',
+		values: [email],
+	};
+
+	const result = await Database.query(query);
+
+	return result.rows.shift();
+};
+module.exports = { usuarios };
