d2c82f25753036e13da1268b1d978e6b2157113e
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-23 23:48:40 -0300

    Funcionalidade: Inserção do Koa-cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const Koa = require("koa");
-const bodyparser = require("koa-bodyparser");
-const router = require("./src/routes");
+const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
+const router = require('./src/routes');
+const cors = require('@koa/cors');
 
 const server = new Koa();
 
+server.use(cors());
 server.use(bodyparser());
 server.use(router.routes());
 
-server.listen(8081, () => console.log("Rodando!"));
+
+server.listen(8081, () => console.log('Rodando!'));
