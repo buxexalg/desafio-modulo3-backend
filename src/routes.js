9a20becd98d79a585b2a9981fcb80dbbf0985078
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-23 23:48:59 -0300

    Funcionalidade: Endpoints definidos através do router.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
-const Router = require("koa-router");
+const Router = require('koa-router');
+const TabelaBrasileirao = require('./controllers/tabelaBrasileirao');
+const Auth = require('./controllers/auth');
+const Session = require('./middlewares/session');
 
 const router = new Router();
 
-/**
- * Definição de rotas
- */
+/* Lista os jogos de uma rodada específica */
+router.get('/jogos/:rodada', TabelaBrasileirao.obterJogos);
+
+/* Altera o placar de um jogo específico. Necessita autorização. */
+router.post('/jogos', Session.verify, TabelaBrasileirao.alterarPlacar);
+
+/* Retorna a classificação final do brasileirão 2019 */
+router.get('/classificacao', TabelaBrasileirao.obterClassificacao);
+
+/* Endpoint de autenticação */
+router.post('/auth', Auth.autenticar);
 
 module.exports = router;
